5ea857a2d86dac08d9c6f932c23fb41fa9e4fe41
Guillaume D <guillaume@trebuchet.fun>
2021-01-29 15:41:22 -0500

    entity

---
+++ Assets/Script/Entity.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class Entity :MonoBehaviour
+{
+    public Vector2Int position;
+    public abstract void Init();
+    public abstract void Step();
+}
---
+++ Assets/Script/MovingEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingEntity : Entity
+{
+
+
+    public override void Init()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Step()
+    {
+        throw new System.NotImplementedException();
+    }
+}
