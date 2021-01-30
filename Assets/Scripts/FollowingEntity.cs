f79f7830e69e655a211556d012ff7029a738d065
Guillaume D <guillaume@trebuchet.fun>
2021-01-30 12:27:06 -0500

    following entity

--- Assets/Scripts/FollowingEntity.cs
+++ Assets/Scripts/FollowingEntity.cs
@@ -6,8 +6,12 @@ public class FollowingEntity : Entity
 {
     public Entity player;
     public int cellPerStep;
+    public int maxFollowingDistance;
+    public int detectionDistance;
 
     private EntityManager manager;
+    private Vector2Int startPosition;
+    private Vector2Int target;
 
     public override void Init()
     {
@@ -17,11 +21,33 @@ public class FollowingEntity : Entity
 
         position.x = (int)(transform.position.x);
         position.y = (int)(transform.position.z);
+
+        startPosition = position;
     }
 
     public override void Step()
     {
-        position += Pathfinding.GetStepMovement(manager, this, player.position, cellPerStep);
-        transform.position = new Vector3(position.x, 0, position.y);
+        Vector2Int distPlayerEntity = player.position - position;
+        Vector2Int distStartPlayer = player.position - startPosition;
+
+        bool followPlayer = distPlayerEntity.magnitude < detectionDistance && distStartPlayer.magnitude < maxFollowingDistance;
+
+        if (followPlayer) 
+        {
+            position += Pathfinding.GetStepMovement(manager, this, player.position, cellPerStep);
+            transform.position = new Vector3(position.x, 0, position.y);
+        }
+        else
+        {
+            if(position != startPosition) 
+            {
+                position += Pathfinding.GetStepMovement(manager, this, startPosition, cellPerStep);
+                transform.position = new Vector3(position.x, 0, position.y);
+            }
+            else 
+            {
+                manager.AddPathNode(new EntityPathNode(this, position));
+            }
+        }
     }
 }
