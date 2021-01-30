9ae8aaaf4a3c3762ebbad1f397451bff5fa2ddcd
franck <franckmorgand@hotmail.fr>
2021-01-31 00:22:45 +0100

    Added entity visibility check

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,7 +4,20 @@ public abstract class Entity : MonoBehaviour
 {
     public EntityType type;
     public Vector2Int position;
+    public bool isVisible;
+    public int visibilityRange;
     public abstract void Init();
     public abstract void Step();
     public virtual void Interact(Entity other) { }
+    public virtual void VisibilityCheck(Entity other)
+    {
+        if(Vector3.Distance( GameManager.Instance.entityManager.Player.transform.position, this.transform.position)< visibilityRange)
+        {
+            isVisible = true;
+        }
+        else
+        {
+            isVisible = false;
+        }
+    }
 }
