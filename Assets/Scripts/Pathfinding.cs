bc8bbeb384995f6f395c0e7043bf4a0955e69f7d
Guillaume D <guillaume@trebuchet.fun>
2021-01-31 11:52:22 -0500

    terrain and collision

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class Pathfinding
 {
+    public static Vector2Int Origin;
+    public static bool[,] CollisionMatrice;
+
+    public static void SetCollision(Vector3 origin, bool[,] colMat) 
+    {
+        Origin = new Vector2Int((int)origin.x, (int)origin.z);
+        CollisionMatrice = colMat;
+    }
+
     public static Vector2Int GetStepMovement(EntityManager manager,  Entity entity, Vector2Int targetPos, int maxCell) 
     {
         Vector2Int move = new Vector2Int();
+        Vector2Int newMove = new Vector2Int();
         Vector2Int dir = targetPos - entity.position;
 
         for(int i = 0; i < maxCell; i++) 
@@ -19,15 +29,24 @@ public class Pathfinding
             if(Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
             {
                 int j = (int)Mathf.Sign(dir.x);
-                move.x += j;
+                newMove.x += j;
                 dir.x -= j;
             }
             else
             {
                 int j = (int)Mathf.Sign(dir.y);
-                move.y += j;
+                newMove.y += j;
                 dir.y -= j;
             }
+
+            Vector2Int matPos = entity.position + newMove - Origin;
+
+            if(CollisionMatrice[matPos.x, matPos.y]) 
+            {
+                return move;
+            }
+
+            move = newMove;
 
             manager.AddPathNode(new EntityPathNode(entity, entity.position + move));
         }
